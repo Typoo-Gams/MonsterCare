ab06417f1a201674da9d54576ab4eb91b8448a54
Charlie <eikascharlie@gmail.com>
2021-01-22 14:27:13 +0100

    Scripts
    My bad boi scripts

---
+++ _Scripts/Battle/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Enemy playerHealth;
+
+    public void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Enemy>();
+        slider = GetComponent<Slider>();
+        slider.maxValue = playerHealth.maxHealth;
+        slider.value = playerHealth.maxHealth;
+    }
+
+    public void SetHealth(int hp)
+    {
+        slider.value = hp;
+    }
+}
