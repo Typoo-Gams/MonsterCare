f6c7e920d83f388c6bb630ac449f8476b8326e3a
Charlie <eikascharlie@gmail.com>
2021-02-10 13:03:38 +0100

    putting in my stuff
    just my scene and scripts im working on, just wanted to have the updated project

---
+++ Attack Prototype/Assets/_Scripts/SpecialAttack_Button/AttackButton_Charlie.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackButton_Charlie : MonoBehaviour
+{
+
+    private Button ThisButton;
+    public MonsterManager_AttackPrototype AttackThisMonster;
+    private int Damage = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisButton = gameObject.GetComponent<Button>();
+        ThisButton.onClick.AddListener(SpecialAttack);
+        AttackThisMonster.StartMonster.DebugStatus();
+    }
+
+    
+    public void SpecialAttack()
+    {
+        AttackThisMonster.StartMonster.UpdateHealth(AttackThisMonster.StartMonster.HealthStatus - Damage);
+    }
+}
---
+++ Attack Prototype/Assets/_Scripts/SpecialAttack_Button/DetectTaps_AttackButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectTaps_AttackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.CompareTag("Monster"))
+                {
+                    Debug.Log("Monster clicked");
+                }
+            }
+        }
+    }
+}
