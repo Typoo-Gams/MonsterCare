c365cf32b72b750efd808515d4343d1f6fdf8a04
roock89 <77836346+roock89@users.noreply.github.com>
2021-02-12 09:36:45 +0100

    swap locations

---
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/SpecialAttack_Button/Cooldown_Ability2.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cooldown_Ability2 : MonoBehaviour
+{
+    
+    public Image monsterIcon;
+    public float Cooldown = 10;
+    
+
+    bool isCooldown = false;
+
+    private int Damage = 10;
+    public MonsterManager_AttackPrototype AttackThisMonster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterIcon.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OurMonsterAttack();
+    }
+
+    void OurMonsterAttack()
+    {
+
+            if (Input.touchCount > 1 && isCooldown == false)
+            {
+                isCooldown = true;
+                monsterIcon.fillAmount = 1;
+                SpecialAttack();
+            }
+
+            if (isCooldown)
+            {
+                monsterIcon.fillAmount -= 1 / Cooldown * Time.deltaTime;
+
+                if (monsterIcon.fillAmount <= 0)
+                {
+                    monsterIcon.fillAmount = 0;
+                    isCooldown = false;
+                }
+            }
+        
+    }
+
+    public void SpecialAttack()
+    {
+        AttackThisMonster.StartMonster.UpdateHealth(AttackThisMonster.StartMonster.HealthStatus - Damage);
+    }
+}
