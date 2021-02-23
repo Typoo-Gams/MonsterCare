7f628b5b550d9f81788fafc19d8c3dbebec3bb5e
roock89 <77836346+roock89@users.noreply.github.com>
2021-02-23 13:54:07 +0100

    more updates to UI for webgl

---
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/Control_Mechanics/DragAndMove_Objects_webGl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndMove_Objects_webGl : MonoBehaviour
+{
+    bool clicked = false;
+
+    private void Update()
+    {
+        //If the gameobject is being touched move the object to the mouse position.
+        if (clicked)
+        {
+            Vector3 direction = Input.mousePosition;
+            direction.z = 0;
+            transform.position = direction;
+        }
+    }
+
+
+    //When this gameobject is beig touched set to true.
+    private void OnMouseDown()
+    {
+        clicked = true;
+    }
+
+
+    //When this gameobject is being let go off set to false.
+    private void OnMouseUp()
+    {
+        clicked = false;
+    }
+}
