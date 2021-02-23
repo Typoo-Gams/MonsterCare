9ee1fdd187006e25ce933374a72337061388f0f9
roock89 <77836346+roock89@users.noreply.github.com>
2021-02-23 16:12:59 +0100

    version number added

--- MonsterCare Mechanics Prototype/Assets/_Scripts/GameManager.cs
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     public GameObject MonsterObject;
     //slider for stuff
     public Slider SliderPrefab;
+    public GameObject versionNumber;
 
     private void OnDestroy()
     {
