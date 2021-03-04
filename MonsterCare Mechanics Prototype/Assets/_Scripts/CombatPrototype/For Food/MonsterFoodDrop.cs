a82bc859019d8d028ec74a7c338440a318c914c6
Charlie <77836344+TheKingChu@users.noreply.github.com>
2021-03-04 19:08:40 +0100

    Update MonsterFoodDrop.cs

--- MonsterCare Mechanics Prototype/Assets/_Scripts/CombatPrototype/For Food/MonsterFoodDrop.cs
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/CombatPrototype/For Food/MonsterFoodDrop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MonsterFoodDrop : MonoBehaviour
@@ -8,14 +9,41 @@ public class MonsterFoodDrop : MonoBehaviour
     GameManager manager;
 
     public GameObject[] foodPrefab;
+    public GameObject[] specialPrefab;
+
     public bool isCreated;
     bool slideBar = true;
+    int element;
 
     // Start is called before the first frame update
     void Start()
     {
+        string scene = SceneManager.GetActiveScene().name;
         manager = GameObject.Find("__app").GetComponentInChildren<GameManager>();
         isCreated = false;
+
+        switch (scene)
+        {
+            case "Savannah_FS":
+                element = 0;
+                break;
+
+            case "Desert_FS":
+                element = 0;
+                break;
+
+            case "Ice_FS":
+                element = 1;
+                break;
+
+            case "Mountain_FS":
+                element = 2;
+                break;
+
+            case "Forest_FS":
+                element = 3;
+                break;
+        }
     }
 
     private void Update()
@@ -43,6 +71,7 @@ public class MonsterFoodDrop : MonoBehaviour
     private void FoodDrop()
     {
         int random = Random.Range(0, foodPrefab.Length);
+        int dropRate = Random.Range(1, 11);
 
         if (isCreated == true)
         {
@@ -52,6 +81,10 @@ public class MonsterFoodDrop : MonoBehaviour
                 Debug.LogWarning("searching inv");
                 if(manager.FoodInventory[i] == null)
                 {
+                    if(dropRate == 1)
+                    {
+                        manager.FoodInventory[i] = Instantiate(specialPrefab[element]);
+                    }
                     manager.FoodInventory[i] = Instantiate(foodPrefab[random]);
                     Destroy(manager.Enemy.gameObject);
                     Destroy(GameObject.FindGameObjectWithTag("CanvasFighting"));
