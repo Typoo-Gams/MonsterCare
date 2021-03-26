71320cbaf10fb743b7367e1a9dc2d50459ca6d87
Charlie <eikascharlie@gmail.com>
2021-03-26 14:14:15 +0100

    notes

---
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/Buttons/NotesReports_Button.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotesReports_Button : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(OpenNotes);
+        gameObject.GetComponent<Button>().onClick.AddListener(OpenReports);
+    }
+
+    public void OpenNotes()
+    {
+        FindObjectOfType<SoundManager>().play("ButtonClick");
+    }
+
+    public void OpenReports()
+    {
+        FindObjectOfType<SoundManager>().play("ButtonClick");
+    }
+}
