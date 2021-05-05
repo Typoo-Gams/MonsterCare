9043ecf5083428051090f39506310afee25101ab
Charlie <77836344+TheKingChu@users.noreply.github.com>
2021-05-05 14:11:13 +0200

    update notes

--- MonsterCare Mechanics Prototype/Assets/_Scripts/Buttons/ChangeFolder_Button.cs
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/Buttons/ChangeFolder_Button.cs
@@ -5,24 +5,5 @@ using UnityEngine.UI;
 
 public class ChangeFolder_Button : MonoBehaviour
 {
-    bool isActive;
-    Image page;
 
-    public void TaskOnClick()
-    {
-        if (!isActive)
-        {
-            page.enabled = true;
-            isActive = true;
-            Debug.LogWarning("enabled page");
-        }
-
-        if (isActive)
-        {
-            page.enabled = false;
-            isActive = false;
-            Debug.LogWarning("disabled page");
-        }
-        
-    }
 }
