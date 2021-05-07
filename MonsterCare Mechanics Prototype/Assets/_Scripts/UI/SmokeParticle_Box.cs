aca1a537241af1906e03bd73b743ec4c83d8fcf1
sketchyyu <juliastokke@gmail.com>
2021-05-07 12:53:45 +0200

    added box animation and fixed spamtapping

--- MonsterCare Mechanics Prototype/Assets/_Scripts/UI/SmokeParticle_Box.cs
+++ MonsterCare Mechanics Prototype/Assets/_Scripts/UI/SmokeParticle_Box.cs
@@ -7,48 +7,73 @@ using UnityEngine.SceneManagement;
 public class SmokeParticle_Box : MonoBehaviour
 {
     public GameObject smokeParticles;
+    public Button[] myButton;
+    public Text[] myText;
     public int SceneNumber = 0;
     public string SceneName = "";
     GameManager manager;
-    float cnt;
+    float fadeAnimCnt = 0;
+    float openAnimCnt = 0;
     Animator Fade;
-
+    Animator Open;
     int tapCounter;
+    bool canTap = true;
 
     private void Start()
     {
         manager = GameObject.Find("__app").GetComponentInChildren<GameManager>();
         Fade = GameObject.FindGameObjectWithTag("Fade").GetComponent<Animator>();
+        Open = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (tapCounter > 2)
         {
-            cnt += Time.deltaTime;
-            Fade.Play("FadeOut");
-            if (cnt > Fade.GetCurrentAnimatorStateInfo(0).length) 
-            {
-                if (SceneName == "")
-                {
-                    SceneManager.LoadScene(SceneNumber);
-                }
-                else
-                {
+            openAnimCnt += Time.deltaTime;
+            Open.SetBool("Tapped", true);
+            canTap = false;
 
-                    SceneManager.LoadScene("LoadScene");
-                    manager.sceneName = "MonsterHome";
-                }
+            foreach (Button found in myButton)
+            {
+                found.gameObject.SetActive(false);
             }
-            
+
+            foreach (Text found in myText)
+            {
+                found.gameObject.SetActive(false);
+            }
+
+            if (openAnimCnt > Open.GetCurrentAnimatorStateInfo(0).length)
+            {
+                fadeAnimCnt += Time.deltaTime;
+                Fade.Play("FadeOut");
+
+                if (fadeAnimCnt > Fade.GetCurrentAnimatorStateInfo(0).length)
+                {
+                    if (SceneName == "")
+                    {
+                        SceneManager.LoadScene(SceneNumber);
+                    }
+                    else
+                    {
+
+                        SceneManager.LoadScene("LoadScene");
+                        manager.sceneName = "MonsterHome";
+                    }
+                }
+            }            
         }
     }
 
     private void OnMouseDown()
     {
-        FindObjectOfType<SoundManager>().play("BoxTapp");
-        Smoke();
-        tapCounter++;
+        if (canTap)
+        {
+            FindObjectOfType<SoundManager>().play("BoxTapp");
+            Smoke();
+            tapCounter++;
+        }
     }
 
     private void Smoke()
